b7e9dffd1f4a913341c7d8a47c2e1fff1c29ab65
Igor Tsygankov <info@itsygankov.com>
2023-08-18 12:27:10 +0300

    style: update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,32 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    "prettier",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "plugin:react/recommended",
-    "plugin:react/jsx-runtime"
+    "plugin:import/recommended"
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "react"],
+  plugins: ["react-refresh", "react", "unused-imports"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true }
+    ],
+    "react/react-in-jsx-scope": "off",
+    "no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_"
+      }
     ]
   },
   parserOptions: {
@@ -22,5 +35,15 @@ module.exports = {
     sourceType: "module",
     project: ["./tsconfig.json", "./tsconfig.node.json"],
     tsconfigRootDir: __dirname
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["./"]
+      }
+    },
+    react: {
+      version: "detect"
+    }
   }
 };
