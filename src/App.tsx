b27027f47962795a11f06f567776fbe9d815d962
Igor Tsygankov <info@itsygankov.com>
2023-08-24 15:37:58 +0300

    feat: update title font

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import SettingsTable from "./components/SettingsTable/SettingsTable";
 const App = () => {
   return (
     <div className='container mx-auto py-12'>
-      <h1 className='mb-8 text-4xl font-bold'>SpeedLink shortcuts</h1>
+      <h1 className='mb-8 text-center font-inria text-4xl font-bold '>
+        speedlink
+      </h1>
       <SettingsTable />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -69,7 +69,8 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out"
-      }
+      },
+      fontFamily: { inria: ["Inria Sans", "sans-serif"] }
     }
   },
   plugins: [require("tailwindcss-animate")]
